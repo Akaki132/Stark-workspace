91c9d1d1b5dad42a9ea458c3e1ddfead0e48525d
Akaki132 <213458766+Akaki132@users.noreply.github.com>
2026-09-10 23:21:27 +0400

    encorporating formal logic symbols

--- NeuroSymbolic_AI/symbolic_table.py
+++ NeuroSymbolic_AI/symbolic_table.py
@@ -1,4 +1,9 @@
-symbolic_table = {}
+symbolic_table_1 = {"Negation" : ["\u00ac", "~"], "Conjunction" : ["∧", "&", "•"],
+                    "Disjunction" : ["∨"], "Condition" : ["\u2192", "\u2283", "\u2287"],
+                    "Biconditional" : ["\u21D4", "\u2194"]}
 
-
-def replace_with_symbols():
+def replace_with_symbols(complete_input):
+    i = 0
+    for symbol in complete_input:
+        if symbol in symbolic_table_1:
+            pass
